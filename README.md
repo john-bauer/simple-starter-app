1d4c4a5db07efc57a57e437466a3455763acb78b
John Bauer <johnbauer126@gmail.com>
2019-03-04 09:37:50 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # simple-starter-app
+Bindings for a basic vue app (no cli) with bulma, scss, and firebase (firestore)
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Starter App',
+        description: 'Simple Bindings for Vue.js, Bulma, Sass, and Firebase (Firestore)'
+    }
+})
